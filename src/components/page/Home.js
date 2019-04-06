5f270cd94c273136c8b0c09463c2d72becf29045
Madeleine Woodbury <randine25@gmail.com>
2019-04-06 16:42:29 -0400

    Styling implemented for the home component. Color changed for global App.css file

--- src/components/page/Home.js
+++ src/components/page/Home.js
@@ -1,18 +1,24 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="container">
-      <h1>Welcome to Your Lists</h1>
-      <p>Keep track of what you need to get done today!</p>
-      <div className="home-links">
-        <a href="/" className="btn">
-          Shopping List
-        </a>
-        <a href="/" className="btn">
-          Todo List
-        </a>
-      </div>
+      <header id="home">
+        <h1>Welcome to Your Lists</h1>
+        <p>
+          Keep track of what you need to get done today! <br />
+          Select a list to get started.
+        </p>
+        <div className="home-links">
+          <a href="/" className="btn">
+            Shopping List
+          </a>
+          <a href="/" className="btn">
+            Todo List
+          </a>
+        </div>
+      </header>
     </div>
   );
 };
